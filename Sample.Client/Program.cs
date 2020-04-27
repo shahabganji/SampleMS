92e39db2cd7e45d38e13979a4cedef4eeb49f086
Saeed Ganji <shahabganji@gmail.com>
2020-04-27 10:44:46 +0430

    init

---
+++ Sample.Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using static System.Console;
+
+namespace Sample.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            
+            var discovery = await Discover(client);
+            if (discovery.IsError) return;
+            
+            var tokenResponse = await GetAccessToken(client, discovery);
+            if(tokenResponse.IsError) return;
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var weatherResponseMessage = await client.GetAsync("https://localhost:6001/weatherforecast");
+
+            if (weatherResponseMessage.IsSuccessStatusCode)
+            {
+                var result = await weatherResponseMessage.Content.ReadAsStringAsync();
+                WriteLine(JArray.Parse(result));
+            }
+            
+            ReadKey();
+        }
+
+        private static async Task<DiscoveryDocumentResponse> Discover(HttpClient client)
+        {
+            var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:7001");
+
+            if (!discovery.IsError) return discovery;
+            
+            WriteLine(discovery.Error);
+            return discovery;
+        }
+
+        private static async Task<TokenResponse> GetAccessToken(HttpClient client, DiscoveryDocumentResponse discovery)
+        {
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discovery.TokenEndpoint,
+                    ClientId = "client1", ClientSecret = "secret", Scope = "Api1"
+                });
+
+            if (tokenResponse.IsError)
+            {
+                WriteLine(tokenResponse.Error);
+                return tokenResponse;
+            }
+
+            WriteLine(tokenResponse.Json);
+            return tokenResponse;
+        }
+    }
+}
---
+++ Sample.Identity/Config.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Test;
+
+namespace Sample.Identity
+{
+    public static class Config
+    {
+        private const string ApiName = "Api1";
+
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource(ApiName, "My Api"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // machine to machine client (from quickstart 1)
+                new Client
+                {
+                    ClientId = "client1",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // no interactive user, use ClientId & secret
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    AllowedScopes = {ApiName}
+                },
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    // interactive user
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login => points to mvc client urls
+                    RedirectUris = {"https://localhost:6002/signin-oidc"},
+
+                    // where to redirect to after logout => points to mvc client urls
+                    PostLogoutRedirectUris = {"https://localhost:6002/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile, 
+                        ApiName // can access the api1
+                    },
+                    
+                    AllowOfflineAccess = true, //enables refresh token
+                },
+            };
+
+
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    Username = "shahab", Password = "ganji",
+                    SubjectId = Guid.NewGuid().ToString()
+                }
+            };
+    }
+}
